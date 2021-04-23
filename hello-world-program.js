1b77302d81f187e636a3bdca5ae7c32484957d39
JacobHornbeck <hor20009@byui.edu>
2021-04-23 10:01:19 -0600

    Add program

---
+++ hello-world-program.js
@@ -0,0 +1,7 @@
+/*****************************************************\
+ * After installing Node.js you can run this program *
+ * in the console by running the following command:  *
+ *      node hello-world-program.js                  *
+\*****************************************************/
+
+console.log("Hello World!")
